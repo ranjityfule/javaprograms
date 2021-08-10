ee7e1198aea48d9be87962486ebea123f0f4b5cf
ranjitfule <88702268+ranjitfule@users.noreply.github.com>
2021-08-10 13:08:09 +0530

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,10 @@
+public class Example{
+  public static void main(String[] args){
+    System.out.println("This is the Example.java file");
+    logs();
+  }
+  public static void logs(){
+    System.out.println("This is the logfile generate method.");
+    System.out.println("This method will create the file which will have all record of DB");
+  }
+}
