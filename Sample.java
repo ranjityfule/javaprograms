8a53da40cf7204e503a84857e0bb647eefb9f6a6
ranjitfule <88702268+ranjitfule@users.noreply.github.com>
2021-08-18 11:22:15 +0530

    added getValue method

--- Sample.java
+++ Sample.java
@@ -3,4 +3,6 @@ public class Sample{
 		//This simple line tells system to print text "Hello World!" on the screen
 		System.out.println("Hello World!");
 	}
+	
+	public static double getValue(double x){ return 1.0; }
 }
